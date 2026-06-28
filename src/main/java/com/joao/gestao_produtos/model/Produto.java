2b47bbcc44bb5860f4a646f8fb5e7424e8368e0e
joaovictormnzs <jvictormenezesl@gmail.com>
2026-06-28 16:02:30 -0300

    feat: entidade produto criada

---
+++ src/main/java/com/joao/gestao_produtos/model/Produto.java
@@ -0,0 +1,31 @@
+package com.joao.gestao_produtos.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name= "tb_produto")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private Integer quantidadeEstoque;
+}
