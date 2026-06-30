bf27c91c4c4c25a0ec17a05f8171e69694e1da28
joaovictormnzs <jvictormenezesl@gmail.com>
2026-06-30 10:30:57 -0300

    feat: adicionando endpoint de atualizar e deletar para completar o CRUD

--- src/main/java/com/joao/gestao_produtos/controller/ProdutoController.java
+++ src/main/java/com/joao/gestao_produtos/controller/ProdutoController.java
@@ -29,10 +29,42 @@ public class ProdutoController {
     // Rota 2: Listar todos os produtos.
     @GetMapping
     public ResponseEntity<List<Produto>> listarTodos() {
-        list<Produto> produtos = produtoRepository.findAll();
+        List<Produto> produtos = produtoRepository.findAll();
 
         // Retorna a lista de produtos com o status HTTP 200 (OK)
         return ResponseEntity.ok(produtos);
     }
 
+    //Rota 3: Atualizar produto.
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> atualizar(@PathVariable Long id, @RequestBody Produto produtoAtualizado) {
+        // @PathVariable captura o ID da URL. findById busca o produto no banco.
+        return produtoRepository.findById(id)
+                .map(produtoExistente -> {
+
+                    // Atualiza os campos do produto antigo com os novos dados recebidos
+                    produtoExistente.setNome(produtoAtualizado.getNome());
+                    produtoExistente.setDescricao(produtoAtualizado.getDescricao());
+                    produtoExistente.setPreco(produtoAtualizado.getPreco());
+                    produtoExistente.setQuantidadeEstoque(produtoAtualizado.getQuantidadeEstoque());
+
+                    // Salva as alterações e retorna 200 OK com o produto atualizado
+                    Produto salvo = produtoRepository.save(produtoExistente);
+                    return ResponseEntity.ok(salvo);
+                })
+
+                // Se o Id nao existe no banco, returna 404 Not Found
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+
+        if (!produtoRepository.existsById(id)) { // existsById checa se o produto realmente existe no banco antes de tentar deletar
+            return ResponseEntity.notFound().build(); // Retorna 404 se não encontrar
+        }
+
+        produtoRepository.deleteById(id);
+        return ResponseEntity.noContent().build(); // Retorna status 204 No Content (Deletado com sucesso)
+    }
 }
